cab6720694c2175d2d2664be6bee8f604fcca9f6
Stephen Belanger <admin@stephenbelanger.com>
2014-07-06 13:35:16 -0700

    Whoops, that should be method, not exports

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ module.exports = method
 function method (method, fn) {
 	return function* (next) {
 		if (this.method.toLowerCase() === method.toLowerCase()) {
-			yield fn.call(this, next)
+			yield* fn.call(this, next)
 		} else {
-			yield next
+			yield* next
 		}
 	}
 }
 
 methods.forEach(function (name) {
-	exports[name] = function (fn) {
+	method[name] = function (fn) {
 		return method(name, fn)
 	}
 })
